3e1efada2c81b2af6c3b782c9984322feacda08a
Bruno <brunobg@gmail.com>
2020-02-19 16:25:46 -0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    BABYLON: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    semi: [2, "always"]
+  }
+}
